52b67489ee699f105a523027baf9207e8e7728b7
mtkramer <kramerm1@mail.usf.edu>
2020-08-06 17:02:25 -0400

    Graded Assignment #3 complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,33 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        if (searchTerm.length() <= 0) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+        return "search.html";
+    }
+
 }
+
+/*
+Use the correct annotation for the method. To configure the correct mapping type and mapping route,
+refer to the form tag in the search.html template.
+The displaySearchResults method should take in a Model parameter.
+The method should also take in two other parameters, specifying the type of search and the search term.
+In order for these last two parameters to be properly passed in by Spring Boot, you need to use the
+correct annotation. Also, you need to name them appropriately, based on the corresponding form field
+names defined in search.html.
+If the user enters “all” in the search box, or if they leave the box empty, call the findAll() method
+from JobData. Otherwise, send the search information to findByColumnAndValue. In either case, store the
+results in a jobs ArrayList.
+Pass jobs into the search.html view via the model parameter.
+Pass ListController.columnChoices into the view, as the existing search handler does.
+ */
